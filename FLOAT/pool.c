b0a6a47412d9e372e8c183807c439493f6cb3410
JarodLCV <jarod.lecoeuvre@etu.unice.fr>
2025-10-10 09:04:23 +0200

    add comment pool.c

--- FLOAT/pool.c
+++ FLOAT/pool.c
@@ -1,14 +1,24 @@
-// pool.c
-// Implémentation des couches de pooling utilisées par lenet_cnn_float.c
-// Max-pooling 2x2, stride 2, no padding
-#include <stdio.h>
-#include "lenet_cnn_float.h" // contient les définitions de dimensions (CONV1_* , POOL1_*, ...)
-
 /**
- * Pool1
- * Input  : conv1_output  [CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH]  (ici 20 x 24 x 24)
- * Output : pool1_output  [POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH]  (ici 20 x 12 x 12)
+ * @file pool.c
+ * @brief Implementation of pooling layers for LeNet CNN in floating-point arithmetic
+ * 
+ * This file contains the implementation of two pooling layers used in the LeNet CNN architecture:
+ * - Pool1: Performs 2x2 max pooling on a 24x24x20 input feature map, producing a 12x12x20 output
+ * - Pool2: Performs 2x2 max pooling on a 8x8x40 input feature map, producing a 4x4x40 output
+ * 
+ * Both pooling layers use:
+ * - Stride of 2
+ * - Window size of 2x2
+ * - Max pooling operation (selecting maximum value in each 2x2 window)
+ * - No padding
  */
+
+#include <stdio.h>
+#include "lenet_cnn_float.h"
+
+/// @brief Pool1
+/// @param conv1_output entries of conv1 layer
+/// @param pool1_output entries of pool1 layer
 void Pool1_24x24x20_2x2x20_2_0(float conv1_output[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH],
                                 float pool1_output[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH])
 {
@@ -16,22 +26,17 @@ void Pool1_24x24x20_2x2x20_2_0(float conv1_output[CONV1_NBOUTPUT][CONV1_HEIGHT][
     for (ch = 0; ch < CONV1_NBOUTPUT; ch++) {
         for (oy = 0; oy < POOL1_HEIGHT; oy++) {
             for (ox = 0; ox < POOL1_WIDTH; ox++) {
-                // coordonnées du coin supérieur gauche de la fenêtre 2x2 dans l'entrée
                 short in_y = oy * 2;
                 short in_x = ox * 2;
 
-                // max pooling 2x2
                 float m = conv1_output[ch][in_y][in_x];
 
-                // élément (in_y, in_x+1)
                 if (conv1_output[ch][in_y][in_x + 1] > m)
                     m = conv1_output[ch][in_y][in_x + 1];
 
-                // élément (in_y+1, in_x)
                 if (conv1_output[ch][in_y + 1][in_x] > m)
                     m = conv1_output[ch][in_y + 1][in_x];
 
-                // élément (in_y+1, in_x+1)
                 if (conv1_output[ch][in_y + 1][in_x + 1] > m)
                     m = conv1_output[ch][in_y + 1][in_x + 1];
 
@@ -41,11 +46,9 @@ void Pool1_24x24x20_2x2x20_2_0(float conv1_output[CONV1_NBOUTPUT][CONV1_HEIGHT][
     }
 }
 
-/**
- * Pool2
- * Input  : conv2_output  [CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH]  (ici 40 x 8 x 8)
- * Output : pool2_output  [POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH]  (ici 40 x 4 x 4)
- */
+/// @brief Pool2
+/// @param conv2_output entries of conv2 layer
+/// @param pool2_output entries of pool2 layer
 void Pool2_8x8x40_2x2x40_2_0(float conv2_output[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH],
                               float pool2_output[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH])
 {
