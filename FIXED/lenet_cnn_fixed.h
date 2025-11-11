362c968c0accabf6fb98a5c8a80df5f065069726
{your_name} <{your_email}>
2025-11-11 14:08:10 +0100

    {your_message}

--- FIXED/lenet_cnn_fixed.h
+++ FIXED/lenet_cnn_fixed.h
@@ -2,40 +2,42 @@
 #ifndef LENET_CNN_FIXED_H
 #define LENET_CNN_FIXED_H
 
+
 #include "lenet_cnn_float.h"  // for dimension constants
+#include "fixed_point.h"
 
 void Conv1_28x28x1_5x5x20_1_0_fixed(
-    short input[IMG_DEPTH][IMG_HEIGHT][IMG_WIDTH],
-    short kernel[CONV1_NBOUTPUT][IMG_DEPTH][CONV1_DIM][CONV1_DIM],
-    short bias[CONV1_NBOUTPUT],
-    short output[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH]);
+    fixed16_16_t input[IMG_DEPTH][IMG_HEIGHT][IMG_WIDTH],
+    fixed16_16_t kernel[CONV1_NBOUTPUT][IMG_DEPTH][CONV1_DIM][CONV1_DIM],
+    fixed16_16_t bias[CONV1_NBOUTPUT],
+    fixed16_16_t output[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH]);
 
 void Pool1_24x24x20_2x2x20_2_0_fixed(
-    short input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH],
-    short output[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH]);
+    fixed16_16_t input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH],
+    fixed16_16_t output[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH]);
 
 void Conv2_12x12x20_5x5x40_1_0_fixed(
-    short input[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH],
-    short kernel[CONV2_NBOUTPUT][POOL1_NBOUTPUT][CONV2_DIM][CONV2_DIM],
-    short bias[CONV2_NBOUTPUT],
-    short output[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH]);
+    fixed16_16_t input[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH],
+    fixed16_16_t kernel[CONV2_NBOUTPUT][POOL1_NBOUTPUT][CONV2_DIM][CONV2_DIM],
+    fixed16_16_t bias[CONV2_NBOUTPUT],
+    fixed16_16_t output[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH]);
 
 void Pool2_8x8x40_2x2x40_2_0_fixed(
-    short input[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH],
-    short output[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH]);
+    fixed16_16_t input[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH],
+    fixed16_16_t output[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH]);
 
 void Fc1_40_400_fixed(
-    short input[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH],
-    short kernel[FC1_NBOUTPUT][POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH],
-    short bias[FC1_NBOUTPUT],
-    short output[FC1_NBOUTPUT]);
+    fixed16_16_t input[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH],
+    fixed16_16_t kernel[FC1_NBOUTPUT][POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH],
+    fixed16_16_t bias[FC1_NBOUTPUT],
+    fixed16_16_t output[FC1_NBOUTPUT]);
 
 void Fc2_400_10_fixed(
-    short input[FC1_NBOUTPUT],
-    short kernel[FC2_NBOUTPUT][FC1_NBOUTPUT],
-    short bias[FC2_NBOUTPUT],
-    short output[FC2_NBOUTPUT]);
+    fixed16_16_t input[FC1_NBOUTPUT],
+    fixed16_16_t kernel[FC2_NBOUTPUT][FC1_NBOUTPUT],
+    fixed16_16_t bias[FC2_NBOUTPUT],
+    fixed16_16_t output[FC2_NBOUTPUT]);
 
-void Softmax_fixed(short input[FC2_NBOUTPUT], float output[FC2_NBOUTPUT]);
+void Softmax_fixed(fixed16_16_t input[FC2_NBOUTPUT], fixed16_16_t output[FC2_NBOUTPUT]);
 
 #endif
